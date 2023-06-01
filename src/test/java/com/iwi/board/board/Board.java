2126eba13947f8da6c1ea1a0963d111edfb1ce7d
eunhyeok <ceh2805@gmail.com>
2023-06-01 15:22:35 +0900

    #게시판 작업
     - 게시판, 댓글 엔티티 작업

---
+++ src/test/java/com/iwi/board/board/Board.java
@@ -0,0 +1,14 @@
+package com.iwi.board.board;
+
+import java.time.LocalDateTime;
+
+public class Board {
+    private Long id;
+    private String title;
+    private String content;
+
+    private LocalDateTime regDt;
+    private String regUser;
+    private LocalDateTime updDt;
+    private String updUser;
+}
